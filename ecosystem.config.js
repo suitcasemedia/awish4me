1d2b9edeb7bfcc8302ac8c964cf544ef54b959e6
jimmy <jimmyjconnelly@gmail.com>
2018-02-26 23:21:56 +0000

    ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+        "script": "yarn",
+        "args" : "start"
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-34-210-87-178.us-west-2.compute.amazonaws.com',
+        key: '~/.ssh/awish4me.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:suitcasemedia/awish4me.git',
+        path: '/home/ubuntu/awish4me',
+        'post-deploy': 'yarn installAll && yarn frontendBuildDeploy && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
+ 
